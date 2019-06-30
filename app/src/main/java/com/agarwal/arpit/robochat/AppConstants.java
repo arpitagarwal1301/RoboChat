fd6b40ca503b5953706edb0b594ee35106f71fb3
Arpit Agarwal - PWA <arpit.agarwal@edelweissfin.com>
2019-06-30 14:28:43 +0530

    Commit 2
    Custom Application class - Timber log library
    SplashScreen with custom theme to remove the white screen at the starting of the app .
    Empty main activity class

---
+++ app/src/main/java/com/agarwal/arpit/robochat/AppConstants.java
@@ -0,0 +1,10 @@
+package com.agarwal.arpit.robochat;
+
+public class AppConstants {
+
+    public static final String apiKey = "6nt5d1nJHkqbkphe";
+    public static final String chatBotID = "63906";
+    public static final String externalID = "arpitvinshu";
+    public static final String hostUrl = "https://www.personalityforge.com/api/chat/";
+
+}
